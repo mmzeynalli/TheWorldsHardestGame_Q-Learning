a78c0d8f028a036c3b8ccc3873511ed6821a1fae
Miradil <33568903+mmzeynalli@users.noreply.github.com>
2018-12-10 00:19:24 +0400

    Delete control.py

--- control.py
+++
@@ -1,8 +0,0 @@
-class ControlPlayer:
-
-    def __init__(self, obj, mode):
-        self.mode = mode
-        self.object = obj
-
-    def moveDown(self):
-        self.object = self.object.move(self.object.speed)
